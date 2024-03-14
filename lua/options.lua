bd99194c38f25cff1ad518a8cec21e300a4769cd
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-14 15:45:07 +0100

    change theme, add relative lines

--- lua/options.lua
+++ lua/options.lua
@@ -5,9 +5,8 @@
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, for help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+-- Relative lines
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
