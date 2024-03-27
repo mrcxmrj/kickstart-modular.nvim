df9ebe58b10790727d2d94af562715a623a3a2d5
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-27 22:35:49 +0100

    add desc

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   'mbbill/undotree',
   opts = {},
   config = function()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndo tree' })
     vim.api.nvim_buf_set_keymap(0, 'n', '<C-n>', '<plug>UndotreeNextState', {})
     vim.api.nvim_buf_set_keymap(0, 'n', '<C-p>', '<plug>UndotreePreviousState', {})
   end,
