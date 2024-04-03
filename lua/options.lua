3fe7e837826d39af5a5855a5d1a48204dfdbd1aa
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-03 17:26:58 +0200

    fix clipboard for wsl

--- lua/options.lua
+++ lua/options.lua
@@ -16,20 +16,18 @@ vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
 
 if IS_WSL then
-  vim.cmd [[
-    let g:clipboard = {
-          \   'name': 'win32yank-wsl',
-          \   'copy': {
-          \      '+': 'win32yank.exe -i --crlf',
-          \      '*': 'win32yank.exe -i --crlf',
-          \    },
-          \   'paste': {
-          \      '+': 'win32yank.exe -o --lf',
-          \      '*': 'win32yank.exe -o --lf',
-          \   },
-          \   'cache_enabled': 0,
-          \ }
-  ]]
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = true,
+}
 end
 
 -- Backup and undo
