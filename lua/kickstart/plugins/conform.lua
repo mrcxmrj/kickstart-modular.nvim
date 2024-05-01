98bfd793f2135387d19c586d4ef1d0e688d7c60a
mrcxmrj <marcin.zurawel@gmail.com>
2024-05-01 15:46:30 +0200

    fix conform for react and typescript

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -21,6 +21,9 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
         go = { 'gofmt' },
         c = { 'clang-format' },
       },
