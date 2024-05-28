e87d02f6513be65a00afda420403472e8a50ff1b
mrcxmrj <marcin.zurawel@gmail.com>
2024-05-28 19:17:42 +0200

    add kemap for incrementing with tmux

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,9 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Remap tmux prefix
+vim.keymap.set('n', '<C-b>', '<C-a>', { desc = 'Increment that works with tmux prefix' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
