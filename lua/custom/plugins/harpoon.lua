b94be8fb25ec00504acf940ce01f7ea7321c2bc8
mrcxmrj <marcin.zurawel@gmail.com>
2024-06-06 17:42:40 +0200

    configure harpoon to save on toggle

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,7 +3,14 @@ return {
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local harpoon = require('harpoon'):setup()
+    local harpoon = require 'harpoon'
+
+    harpoon:setup {
+      settings = {
+        save_on_toggle = true,
+      },
+    }
+
     vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():add()
     end, { desc = '[H]arpoon [A]dd mark' })
@@ -23,36 +30,5 @@ return {
     vim.keymap.set('n', '<leader>h4', function()
       harpoon:list():select(4)
     end, { desc = 'Mark 4' })
-
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<C-S-p>', function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<C-S-n>', function()
-      harpoon:list():next()
-    end)
-
-    -- basic telescope configuration
-    -- local conf = require('telescope.config').values
-    -- local function toggle_telescope(harpoon_files)
-    --   local file_paths = {}
-    --   for _, item in ipairs(harpoon_files.items) do
-    --     table.insert(file_paths, item.value)
-    --   end
-    --
-    --   require('telescope.pickers')
-    --     .new({}, {
-    --       prompt_title = 'Harpoon',
-    --       finder = require('telescope.finders').new_table {
-    --         results = file_paths,
-    --       },
-    --       previewer = conf.file_previewer {},
-    --       sorter = conf.generic_sorter {},
-    --     })
-    --     :find()
-    -- end
-    -- vim.keymap.set('n', '<C-e>', function()
-    --   toggle_telescope(harpoon:list())
-    -- end, { desc = 'Open harpoon window' })
   end,
 }
