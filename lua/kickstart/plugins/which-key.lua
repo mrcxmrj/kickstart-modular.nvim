1daa6f55c97ab70d6110ba5d3a1c7edb93e525c1
mrcxmrj <marcin.zurawel@gmail.com>
2024-06-13 14:00:45 +0200

    add more options to gitsigns

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -27,7 +27,14 @@ return {
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>gt'] = { name = 'Git [T]oggle', _ = 'which_key_ignore' },
+        ['<leader>gh'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
+      -- visual mode
+      require('which-key').register({
+        ['<leader>gh'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
     end,
   },
 }
