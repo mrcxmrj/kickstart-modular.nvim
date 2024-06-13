a2433f77fc5155e8fe73eab4e9aab201295321b5
mrcxmrj <marcin.zurawel@gmail.com>
2024-06-13 14:57:54 +0200

    add inlay hints toggle

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -28,7 +28,7 @@ return {
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>gt'] = { name = 'Git [T]oggle', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>gh'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
       -- visual mode
