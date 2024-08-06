3f6879fd885a5a2f7776d890ec9105c4ed76aeac
Marcin Żurawel <mzurawel@splunk.com>
2024-08-06 09:41:28 +0200

    add keymaps to oil to be more in line with mini.files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,6 +1,9 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = { keymaps = {
+    ['l'] = 'actions.select',
+    ['h'] = 'actions.parent',
+  } },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
     { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
