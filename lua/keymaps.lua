521c153a8b547af88ff514fd12e4a81ec4bc565e
mrcxmrj <marcin.zurawel@gmail.com>
2024-08-16 23:11:11 +0200

    change diagnostic jumping keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,8 +5,8 @@
 vim.keymap.set('n', '<C-b>', '<C-a>', { desc = 'Increment that works with tmux prefix' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic [E]rror message' })
+vim.keymap.set('n', ']e', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic [E]rror message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
